bf704c13b58c595e44836302014ea494d05ce83f
Bruno Maia <bruno1berto@hotmail.com>
2020-05-02 01:37:18 -0300

    SellerService and Basic FindAll

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService; // declaração de dependência para a classe SellerService
+
+        public SellersController(SellerService sellerService) // Construtor injeta a dependência
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // variável recebe uma lista de objetos do sellerService e injeta na view
+            var list = _sellerService.FindAll(); 
+            return View(list);
         }
     }
 }
